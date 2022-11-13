dfcc7522192272f0a2408b365345d904fe918642
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-11-13 23:57:05 +0900

    CTFのテストにコマンドのチェックを追加

--- src/ctf/test-project/test.sh
+++ src/ctf/test-project/test.sh
@@ -12,7 +12,16 @@ source test-utils.sh vscode
 # check "<label>" command goes here
 # checkOSPackages  "<label>" package list goes here
 # checkMultiple "<label>" condition1 condition2
-check "git" git --version
+check "curl" which curl
+check "exiftool" which exiftool
+check "file" which file
+check "ftp" which ftp
+check "g++" which g++
+check "gcc" which gcc
+check "gdb" which gdb
+check "make" which make
+check "pip3" which pip3
+check "python3" which python3
 
 # Report result
 reportResults
