39d9c9954b07681a5b6395349762f165c88554de
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-11-13 23:03:20 +0900

    CTFのテスト用スクリプトを追加

---
+++ src/ctf/test-project/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+source test-utils.sh vscode
+
+# Run common tests
+# checkCommon
+
+# Run definition specific tests
+
+# checkExtension "<Extension ID goes here>"
+# check "<label>" command goes here
+# checkOSPackages  "<label>" package list goes here
+# checkMultiple "<label>" condition1 condition2
+check "git" git --version
+
+# Report result
+reportResults
