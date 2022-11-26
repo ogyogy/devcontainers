266c9b1c0df4e9bdd2a05c4c9ee9add752beeb63
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-11-26 18:33:21 +0900

    update test.sh

--- src/ctf/test-project/test.sh
+++ src/ctf/test-project/test.sh
@@ -4,14 +4,17 @@ cd $(dirname "$0")
 source test-utils.sh vscode
 
 # Run common tests
-# checkCommon
+checkCommon
 
 # Run definition specific tests
 
 # checkExtension "<Extension ID goes here>"
 # check "<label>" command goes here
 check "autopep8" which autopep8
+check "file" which file
 check "flake8" which flake8
+check "objdump" which objdump
+check "strings" which strings
 # checkOSPackages  "<label>" package list goes here
 PACKAGE_LIST="exiftool \
     ftp \
